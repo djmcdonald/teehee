6b4d93c6bd46bb81a21729a995411e24d116d935
djmcdonald <duncan.mcdonald@bbc.co.uk>
2016-07-26 09:31:58 +0100

    Moves servo depending on build status

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var argv = require('minimist')(process.argv.slice(2)),
     includes = require('lodash/includes'),
     jenkins = require('./lib/jenkins'),
+    pow = require('./lib/pow'),
     slackHook = argv.s,
     slack = require('./lib/slack')(slackHook),
     jenkinsHost = argv.j,
@@ -10,30 +11,45 @@ var argv = require('minimist')(process.argv.slice(2)),
     ca = argv.a,
     cert = argv.c,
     key = argv.k,
-    builds = [];//,
-    //pow = require('./lib/pow');
+    builds = [];
 
 jenkins.setup(jenkinsHost, project, ca, cert, key);
 
-setInterval(
-    function () {
-        jenkins.get().then(function (latest) {
-            if (includes(builds, latest.lastBuild)) {
-                console.log('Already processed build ' + latest.lastBuild);
-            } else {
-                var msg = 'Prepare to be slapped :poop:';
-                builds.push(latest.lastBuild);
-                console.log('Processing ' + latest.lastBuild);
+pow().then(function (boof) {
+    setInterval(
+        function () {
+            jenkins.get()
+            .then(function (latest) {
+                if (includes(builds, latest.lastBuild)) {
+                    var res = latest;
+                    res.doSlap = false;
+                    console.log('Already processed build ' + latest.lastBuild);
 
-                if (latest.success) {
-                    msg = 'Nae bother, pal - build passed :angel:';
+                    return res;
+                } else {
+                    var msg = 'Prepare to be slapped :poop:',
+                    res = latest;
+                    res.doSlap = true;
+                    builds.push(latest.lastBuild);
+                    console.log('Processing ' + latest.lastBuild);
+
+                    if (latest.success) {
+                        msg = 'Nae bother, pal - build passed :angel:';
+                        res.doSlap = false;
+                    }
+
+                    return slack(msg).then(function () {
+                        return res;
+                    });
                 }
-                
-                return slack(msg).then(function () {
-                    return latest;
-                });
-            }
-        })
-    },
-    2000
-);
+            })
+            .then(function (buildStatus) {
+                if (buildStatus.doSlap) {
+                    console.log('Implementing slap...');
+                    return boof();
+                }
+            });
+        },
+        2000
+    );
+});
